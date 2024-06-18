223db01fc5b7b8ef4b2f513859688c4481096f5f
wirefailed <junsoo.kim.college@gmail.com>
2024-06-17 21:36:01 -0700

    Add a structure of main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from scraper import *
+from education_background import education_background
+
+def main():
+    USC_professors_infos = []
+    # some function(USC_professors_infos)
+    return 0
+
+if __name__ == "__main__":
+    main()
