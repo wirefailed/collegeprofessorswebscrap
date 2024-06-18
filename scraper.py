f6850ee38f72a6143548d86bfdf7f800b4256191
wirefailed <junsoo.kim.college@gmail.com>
2024-06-17 22:27:39 -0700

    Last Save for the day June 17

--- scraper.py
+++ scraper.py
@@ -1,23 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-# from education_background import education_background 
+from education_background import education_background 
 
 def USC_scraper():
      r = requests.get("https://viterbi.usc.edu/directory/faculty/")
      list_URL = []
 
-      
+          
      soup = BeautifulSoup(r.content, 'html.parser')
 
      allFaculty_div = soup.find('div', id = 'results')
      allFaculty_a = allFaculty_div.find_all('a')
 
      for professor_link in allFaculty_a:
-          list_URL(professor_link['href'])
+          list_URL.append(professor_link['href'])
           
-     
-     for URL in list_URL:
-          print(URL)
-          
-         # education_background('https://viterbi.usc.edu/directory/faculty/' + URL, main_matrix)
+
+     for URL in list_URL: 
+          education_background('https://viterbi.usc.edu/directory/faculty/' + URL, main_matrix)
+
+
 
