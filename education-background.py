acfec8c0bcca2bc8411f17c2cade1b10f7eae52a
wirefailed <junsoo.kim.college@gmail.com>
2024-06-17 21:13:15 -0700

    incoporate comments on how to add in database abd other infos

--- education-background.py
+++ education-background.py
@@ -5,6 +5,19 @@ def education_background(id, URL):
     prof_URL = requests.get(URL)
 
     soup = BeautifulSoup(prof_URL.content, 'html.parser')
+
+    # store professor's name and academic title in database
+    professor_info = soup.find('div', class_ = "profileTopRight")
+    professor_name = professor_info.find('h1', class_= 'facultyname').text
+    professor_academic_title = professor_info.find('p', class_='faculty_academic_title').text
+    # db[1] = professor_name 
+    # db[2] = professor_academic_title
+
+    # store professor's degree infos
     degree_background = soup.find_all('div', class_ ='education-piece')
+    database_count = 3
     for degree in degree_background:
         degree_university = degree.li.text
+        #db[database_count] = degree_university
+        database_count += 1
+    
