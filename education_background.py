49c61dc90f6322aa9f1b3e2c5341515ea2de8ab4
Phong Nguyen <phongn102004@gmail.com>
2024-06-18 06:20:31 -0700

    Fixed double occurence of professors in scraper.py and appending the correct URLs

--- education_background.py
+++ education_background.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-def education_background(id,URL)
+def education_background(URL):
+    
     prof_URL = requests.get(URL)
 
     soup = BeautifulSoup(prof_URL.content, 'html.parser')
 
     # store professor's name and academic title in database
     professor_info = soup.find('div', class_ = "contentDetail faculty-directory page-faculty")
-    professor_name = professor_info.find('h4').text
+    professor_name = professor_info.find('h4', class_= "facultyname").text
     professor_academic_title = professor_info.find('p').text
 
     # store professor's degree infos
@@ -20,5 +21,4 @@ def education_background(id,URL)
         degrees.append(li.text)
         
     professorBackground = [professor_name, professor_academic_title, degrees]
-
     return professorBackground
--- scraper.py
+++ scraper.py
@@ -2,22 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 from education_background import education_background 
 
-def USC_scraper():
-     r = requests.get("https://viterbi.usc.edu/directory/faculty/")
-     list_URL = []
+def USC_scraper(mainMatrix):
+    r = requests.get("https://viterbi.usc.edu/directory/faculty/")
+    list_URL = []
+        
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-          
-     soup = BeautifulSoup(r.content, 'html.parser')
+    allFaculty_div = soup.find('div', id = 'results')
+    allFaculty_a = allFaculty_div.find_all('div', class_ = 'faculty-member')
 
-     allFaculty_div = soup.find('div', id = 'results')
-     allFaculty_a = allFaculty_div.find_all('a')
+    for professor_link in allFaculty_a:
+        list_URL.append(professor_link.find('div', class_ = 'faculty-text').find('a')['href'])     
 
-     for professor_link in allFaculty_a:
-          list_URL.append(professor_link['href'])
-          
+    for URL in list_URL:
+        mainMatrix.append(education_background('https://viterbi.usc.edu' + URL))
 
-     for URL in list_URL: 
-          education_background('https://viterbi.usc.edu/directory/faculty/' + URL, main_matrix)
 
 
 
