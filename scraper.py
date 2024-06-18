aa1110971ef85c47f63aafcab30fdd375d510301
wirefailed <junsoo.kim.college@gmail.com>
2024-06-17 21:57:11 -0700

    Get href from the website

--- scraper.py
+++ scraper.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 
 r = requests.get("https://viterbi.usc.edu/directory/faculty/")
 
-print(r)
+#print(r)
 
 soup = BeautifulSoup(r.content, 'html.parser')
-# print(soup.prettify())
 
-allFaculty = soup.find_all('div', class_ = 'id')
 
-for a in allFaculty:
-    print(a["href"])
+allFaculty_div = soup.find('div', id = 'results')
+allFaculty_a = allFaculty_div.find_all('a')
+
+for href in allFaculty_a:
+     print(href['href'])
