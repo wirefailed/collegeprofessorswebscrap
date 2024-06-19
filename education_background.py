66ba909f9180aa29cf25e1a130d2a523de99e982
Phong Nguyen <phongn102004@gmail.com>
2024-06-19 00:50:22 -0700

    Semi-runnable code, brute forced all the broken URLs

--- education_background.py
+++ education_background.py
@@ -9,16 +9,22 @@ def education_background(URL):
 
     # store professor's name and academic title in database
     professor_info = soup.find('div', class_ = "contentDetail faculty-directory page-faculty")
-    professor_name = professor_info.find('h4', class_= "facultyname").text
-    professor_academic_title = professor_info.find('p').text
 
-    # store professor's degree infos
-    degree_background = soup.find('div', class_ ='education-piece')
-    degrees = []
-    for li in degree_background.ul:
-        if li.text == '':
-            break
-        degrees.append(li.text)
-        
-    professorBackground = [professor_name, professor_academic_title, degrees]
-    return professorBackground
+    # Try-catch used to prevent one invalid URL to cause the script to crash
+    try:
+        professor_name = professor_info.find('h4', class_= "facultyname").text
+        professor_academic_title = professor_info.find('p').text
+
+        # store professor's degree infos
+        degree_background = soup.find('div', class_ ='education-piece')
+        degrees = []
+        for li in degree_background.ul:
+            if li.text == '':
+                break
+            degrees.append(li.text)
+            
+        professorBackground = [professor_name, professor_academic_title, degrees]
+        return professorBackground
+    except:
+        # Could possibly append broken URLs here to repeat them back in the calling function
+        return False
--- scraper.py
+++ scraper.py
@@ -11,20 +11,19 @@ def USC_scraper(mainMatrix):
     allFaculty_div = soup.find('div', id = 'results')
     allFaculty_a = allFaculty_div.find_all('div', class_ = 'faculty-member')
 
-    unique_URLs = set()
-
     for professor_link in allFaculty_a:
-          href_URL = professor_link.find('div', class_ = 'faculty-text').find('a')['href']
-          # hashset to only get uniquearray
-          if href_URL and href_URL not in unique_URLs:
-               unique_URLs.append(href_URL)
-               list_URL.append(href_URL)   
-          else:
-               print(f"No href found or duplicate href: {href_URL}")  
+        list_URL.append(professor_link.find('div', class_ = 'faculty-text').find('a')['href'])     
 
     for URL in list_URL:
-        mainMatrix.append(education_background('https://viterbi.usc.edu' + URL))
+        # test to see if the URL is valid
+        testURL = requests.get('https://viterbi.usc.edu' + URL)
+        if(testURL.ok == False):
+            continue
 
-
-
-
+        returnInfo = education_background('https://viterbi.usc.edu' + URL)
+        
+        # Used to check if the returned matrix is valid
+        if(returnInfo == False):
+            continue
+        else:
+            mainMatrix.append(returnInfo)
