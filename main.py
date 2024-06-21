c60113b9a8558da6964a932fe52e40961623e33a
wirefailed <junsoo.kim.college@gmail.com>
2024-06-20 23:12:05 -0700

    Add PostGresSQL database

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 from scraper import USC_scraper
 from education_background import education_background
+import psycopg2
 
 def main():
     USC_professors_infos = []
-    missedProfessors = []
-    USC_scraper(USC_professors_infos, missedProfessors)
+    USC_scraper(USC_professors_infos)
     # sorts USC_professors_infos based off its name order
     sorted(USC_professors_infos, key=lambda x: x[0])
-
-    # to check if it works 
-    for i in USC_professors_infos:
-        print(i)
     
+    # to check if it works 
+    # for i in USC_professors_infos:
+        # print(i)
+
+    print("What is your postgres password?")
+    passWord  = input()
+
+    conn = psycopg2.connect(host="localhost", dbname="collegeprofessorinfos", user="postgres", 
+                            password=passWord, port=5432)
+
+    cur = conn.cursor()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS professor (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        academic_title VARCHAR(100),
+    )""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS degrees (
+        id SERIAL PRIMARY KEY,
+        prof_name VARCHAR(100) NOT NULL,
+        degree VARCHAR(100),
+        degree VARCHAR(100),
+        degree VARCHAR(100),
+        degree VARCHAR(100),
+        degree VARCHAR(100),
+        FOREIGN KEY(prof_name) REFERENCES professor(name)
+    )""")
+
+    for id in range(len(USC_professors_infos)):
+        cur.execute("""INSERT INTO professor(id, name, academic title) VALUES
+        ()
+        """)
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
     return 0
 
 if __name__ == '__main__':
