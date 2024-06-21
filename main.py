f314b1881c3d0aaacdeb5357fc038bc6c7aa38cf
wirefailed <junsoo.kim.college@gmail.com>
2024-06-20 21:19:22 -0700

    add sorted function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from education_background import education_background
 def main():
     USC_professors_infos = []
     USC_scraper(USC_professors_infos)
+    # sorts USC_professors_infos based off its name order
+    sorted(USC_professors_infos, key=lambda x: x[0])
 
     # to check if it works 
     for i in USC_professors_infos:
