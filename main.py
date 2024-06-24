dcda31550e7f7c605476b9f73169e7d90fa3b845
wirefailed <junsoo.kim.college@gmail.com>
2024-06-23 23:00:16 -0700

    add not working for loop in main rn

--- main.py
+++ main.py
@@ -36,36 +36,40 @@ def main():
         cur.execute("""
             CREATE TABLE IF NOT EXISTS professor (
                 id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL,
-                academic_title VARCHAR(100)
+                name VARCHAR(255) NOT NULL,
+                academic_title VARCHAR(255)
             );
-            """)
+        """)
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS degrees (
                 id SERIAL PRIMARY KEY,
-                professor_name VARCHAR(100) NOT NULL,
-                degree VARCHAR(100),
+                professor_id INTEGER NOT NULL,
+                degree VARCHAR(255),
                 FOREIGN KEY (professor_id) REFERENCES professor(id)
             );
-            """)
+        """)
 
         print("Inserting datas into the database")
 
-        for i in range(len(USC_professors_infos)):
-            professor_name = USC_professors_infos[i][0]
-            professor_academic_title = USC_professors_infos[i][1]
-            cur.execute("""INSERT INTO professor(name, academic title) VALUES (%s, %s)""", 
+        for professor_info in USC_professors_infos:
+            professor_name = professor_info[0]
+            professor_academic_title = professor_info[1]
+
+            cur.execute("""INSERT INTO professor(name, academic_title) VALUES (%s, %s) RETURNING id""", 
             (professor_name, professor_academic_title))
-            for j in range(len(USC_professors_infos[i][2])):
-                degree = USC_professors_infos[i][2][j]
-                cur.execute("""INSERT INTO degrees(prof_name, degree) VALUES(%s, %s)""",
-                (professor_name, degree))
+
+            professor_id = cur.fetchone()[0]
+
+            for degree_info in professor_info[2]:
+                cur.execute("""INSERT INTO degrees(professor_id, degree) VALUES(%s, %s)""",
+                (professor_id, degree_info))
 
         conn.commit()
     
     except psycopg2.OperationalError as e:
         print(f"An error occurred: {e}")
+        conn.rollback()
 
     cur.close()
     conn.close()
