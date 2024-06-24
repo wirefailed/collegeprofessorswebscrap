8cba15ee92b4042231d1ed56062652578548636e
wirefailed <junsoo.kim.college@gmail.com>
2024-06-23 22:33:26 -0700

    add some working codes on main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraper import USC_scraper
 from education_background import education_background
 import psycopg2
+from getpass import getpass
 
 def main():
     USC_professors_infos = []
@@ -17,40 +18,54 @@ def main():
         # print(i)
 
     print("Connecting to data base")
-    print("What is your postgres password?")
-    passWord  = input()
+    print("Write your password")
+    passWord  = getpass()
 
-    conn = psycopg2.connect(host="localhost", dbname="collegeprofessorinfos", user="postgres", 
-                            password=passWord, port=5432)
+    try: 
+        conn = psycopg2.connect(
+            host="localhost", 
+            dbname="collegeprofessorinfos", 
+            user="postgres", 
+            password=passWord, 
+            port=5432
+        )
+        cur = conn.cursor()
 
-    cur = conn.cursor()
+        print("Well connected, creating or checking if table exists")
 
-    print("Well connected, creating or checking if table exists")
-    cur.execute("""CREATE TABLE IF NOT EXISTS professor (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        academic_title VARCHAR(100)
-    );""")
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS professor (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                academic_title VARCHAR(100)
+            );
+            """)
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS degrees (
-        id SERIAL PRIMARY KEY,
-        prof_name VARCHAR(100) NOT NULL,
-        degree VARCHAR(100),
-        FOREIGN KEY(prof_name) REFERENCES professor(name)
-    );""")
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS degrees (
+                id SERIAL PRIMARY KEY,
+                professor_id INTEGER NOT NULL,
+                degree VARCHAR(100),
+                FOREIGN KEY (professor_id) REFERENCES professor(id)
+            );
+            """)
 
-    print("Inserting datas into the database")
-    for i in range(len(USC_professors_infos)):
-        professor_name = USC_professors_infos[i][0]
-        professor_academic_title = USC_professors_infos[i][1]
-        cur.execute("""INSERT INTO professor(name, academic title) VALUES (%s, %s)""", 
-        (professor_name, professor_academic_title))
-        for j in range(len(USC_professors_infos[i][2])):
-            degree = USC_professors_infos[i][2][j]
-            cur.execute("""INSERT INTO degrees(prof_name, degree) VALUES(%s, %s)""",
-            (professor_name, degree))
+        print("Inserting datas into the database")
 
-    conn.commit()
+        for i in range(len(USC_professors_infos)):
+            professor_name = USC_professors_infos[i][0]
+            professor_academic_title = USC_professors_infos[i][1]
+            cur.execute("""INSERT INTO professor(name, academic title) VALUES (%s, %s)""", 
+            (professor_name, professor_academic_title))
+            for j in range(len(USC_professors_infos[i][2])):
+                degree = USC_professors_infos[i][2][j]
+                cur.execute("""INSERT INTO degrees(prof_name, degree) VALUES(%s, %s)""",
+                (professor_name, degree))
+
+        conn.commit()
+    
+    except psycopg2.OperationalError as e:
+        print(f"An error occurred: {e}")
 
     cur.close()
     conn.close()
