bae44214df9cd6874ac0fe462b241ff0f7ae262a
wirefailed <junsoo.kim.college@gmail.com>
2024-06-24 16:32:42 -0700

    Complete searching algorithm

--- searchingalgorithm.py
+++ searchingalgorithm.py
@@ -1,22 +1,36 @@
 import psycopg2
+from getpass import getpass
 
 def searching_algorithm(search_term):
-    print("What is your postgres password?")
-    passWord  = input()
+    print("Write your PostgresSQL password")
+    passWord  = getpass()
 
-    conn = psycopg2.connect(host="localhost", dbname="collegeprofessorinfos", user="postgres", 
-                            password=passWord, port=5432)
+    try: 
+        conn = psycopg2.connect(
+            host="localhost", 
+            dbname="collegeprofessorinfos", 
+            user="postgres", 
+            password=passWord, 
+            port=5432
+        )
+        cur = conn.cursor()
 
-    cur = conn.cursor()
+        query = """SELECT p.name, p.academic_title, d.degree
+                FROM professor p
+                JOIN degrees d
+                ON p.id = d.professor_id
+                WHERE d.degree LIKE %s
+            """        
 
-    query = """SELECT id, name
-                FROM degrees
-                WHERE degree LIKE %s
-            """
+        cur.execute(query, ('%' + search_term + '%',))
 
-    cur.execute(query, ('%' + search_term + '%',))
+        results = cur.fetchall()
 
-    results = cur.fetchall()
+        conn.commit()
+    
+    except psycopg2.OperationalError as e:
+        print(f"An error occurred: {e}")
+        conn.rollback()
 
     cur.close()
     conn.close()
@@ -24,3 +38,6 @@ def searching_algorithm(search_term):
     return results
 
 search_term = input("Enter a partial academic title to search for: ")
+results = searching_algorithm(search_term)
+for row in results:
+    print(f"Name: {row[0]}, Title: {row[1]}, Degree Details: {row[2]}")
