bfdc95a5c6cb0a6be2ae36a51d2e55af2986f1f8
wirefailed <junsoo.kim.college@gmail.com>
2024-06-23 22:29:39 -0700

    add postgresSQl test run code

--- testrun.py
+++ testrun.py
@@ -1,28 +1,39 @@
 import psycopg2
+from getpass import getpass
 
-passWord  = input()
+print("Write your password")
+passWord  = getpass()
 
-conn = psycopg2.connect(host="localhost", dbname="collegeprofessorinfos", user="postgres", 
-                        password=passWord, port=5432)
+try: 
+    conn = psycopg2.connect(
+        host="localhost", 
+        dbname="collegeprofessorinfos", 
+        user="postgres", 
+        password=passWord, 
+        port=5432
+    )
+    cur = conn.cursor()
 
-cur = conn.cursor()
+    print("Well connected, creating or checking if table exists")
 
-print("Well connected, creating or checking if table exists")
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS professor (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            academic_title VARCHAR(100)
+        );
+        """)
 
-cur.execute("""CREATE TABLE IF NOT EXISTS professor (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    academic_title VARCHAR(100)
-);""")
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS degrees (
+            id SERIAL PRIMARY KEY,
+            professor_id INTEGER NOT NULL,
+            degree VARCHAR(100),
+            FOREIGN KEY (professor_id) REFERENCES professor(id)
+        );
+        """)
 
-cur.execute("""CREATE TABLE IF NOT EXISTS degrees (
-    id SERIAL PRIMARY KEY,
-    prof_name VARCHAR(100) NOT NULL,
-    degree VARCHAR(100),
-    FOREIGN KEY(prof_name) REFERENCES professor(name)
-);""")
-
-print("Inserting datas into the database")
+    print("Inserting datas into the database")
 
 # for i in range(len(USC_professors_infos)):
         # professor_name = USC_professors_infos[i][0]
@@ -33,8 +44,11 @@ print("Inserting datas into the database")
             # degree = USC_professors_infos[i][2][j]
             # cur.execute("""INSERT INTO degrees(prof_name, degree) VALUES(%s, %s)""",
             # (professor_name, degree))
+            
+    conn.commit()
 
-conn.commit()
+except psycopg2.OperationalError as e:
+    print(f"An error occurred: {e}")
 
 cur.close()
 conn.close()
